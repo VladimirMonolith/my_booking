a216bf6b0402e2c4db1816fdc377eb78b0281734
Гут Владимир <vladimir.monolith@yandex.ru>
2023-04-22 07:43:18 +0300

    Доработаны все эндпойнты

--- app/rooms/dao.py
+++ app/rooms/dao.py
@@ -1,9 +1,4 @@
-from datetime import date
-
-from sqlalchemy import select
-
 from app.dao.base import BaseDAO
-from app.database.connection import async_session_maker
 
 from .models import Room
 
--- app/rooms/router.py
+++ app/rooms/router.py
@@ -1,4 +1,3 @@
-from datetime import date
 from typing import List
 
 from fastapi import APIRouter
